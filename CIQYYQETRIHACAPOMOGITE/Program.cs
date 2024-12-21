c3a32840a69a8e3b12fa9c1eb1bc26d60d7539ff
Надежда <@DESKTOP-EHSPKBR>
2024-12-21 22:08:28 +0200

    Добавьте файлы проекта.

---
+++ CIQYYQETRIHACAPOMOGITE/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class TimeSpanConverter
+{
+    
+    public int Minutes { get; set; }
+    public int Seconds { get; set; }
+
+    
+    public TimeSpanConverter(int minutes, int seconds)
+    {
+        Minutes = minutes;
+        Seconds = seconds;
+    }
+
+    
+    public int TotalSeconds()
+    {
+        return (Minutes * 60) + Seconds;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+    
+        Console.Write("Введите количество минут: ");
+        int minutes = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество секунд: ");
+        int seconds = int.Parse(Console.ReadLine());
+
+        
+        TimeSpanConverter timeSpan = new TimeSpanConverter(minutes, seconds);
+
+        
+        int totalSeconds = timeSpan.TotalSeconds();
+        Console.WriteLine($"Общее количество секунд: {totalSeconds}");
+    }
+}
